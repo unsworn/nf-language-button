8115b1fc1e0fca284cfd9f287bab79651033f1bc
erik <erik@unswornindustries.com>
2015-10-07 12:50:27 +0200

    Automatic and manual engagement gestures

--- README.md
+++ README.md
@@ -4,9 +4,15 @@ Arduino sketch powering the [nf-birdstories](https://github.com/unsworn/nf-birds
 
 ## Functionality
 
-When a user pushes the button the <kbd>l</kbd> is sent.
+### Language button
 
-More here about periodical <kbd>p</kbd> events...
+When a user pushes the button the <kbd>l</kbd> key is clicked and released by the Arduino (software) keyboard. This 
+
+### Pointer lock engagement gesture
+
+Also, to enable pointer lock on nf-birdstories the <kbd>p</kbd> key is triggered every 10 seconds for 10 minutes after bootup.
+
+Administrators can manually trigger the engagement gesture by pressing and holding the language button for 3 seconds. This will also cancel the automatic, recurring engagement gesture sequence.
 
 ## Notes
 
--- nf-language-button.ino
+++ nf-language-button.ino
@@ -1,84 +1,108 @@
 /*
- Input Pullup Serial
+ nf-language-button
 
- This example demonstrates the use of pinMode(INPUT_PULLUP). It reads a
- digital input on pin 2 and prints the results to the serial monitor.
+ See README.md in the git repo for details.
+*/
 
- The circuit:
- * Momentary switch attached from pin 2 to ground
- * Built-in LED on pin 13
+// Pins used
+const int BUTTON_PIN = 2;
+const int LED_PIN = 13;
 
- Unlike pinMode(INPUT), there is no pull-down resistor necessary. An internal
- 20K-ohm resistor is pulled to 5V. This configuration causes the input to
- read HIGH when the switch is open, and LOW when it is closed.
+// How many milliseconds is considered a long press
+const int LONG_PRESS_DURATION = 3000;
 
- */
+// This character is sent on button press
+const char LANGUAGE_CHAR = 'l';
 
-//#include "Keyboard.h"
+// This character is sent as an engagement gesture after bootup
+const char ENGAGEMENT_CHAR = 'p';
 
-int buttonPin = 2;
+// Keep sending 'p' for 10 minutes after bootup (1000 * 60 * 10)
+const long ENGAGEMENT_DURATION = 600000; 
+
+// Send 'p' after bootup every 10 seconds (1000 * 10)
+const long ENGAGEMENT_INTERVAL =  10000; 
+
+// Variables
+boolean autoEngagementEnabled = true;
+boolean longPressEngagementEnabled = true;
+unsigned long elapsedTime = 0;
+unsigned long lastEngagementTime = 0;
+unsigned long lastPressTime = 0;
 int previousButtonState = LOW;
+int buttonState;
 
-void setup() {
-  
+void setup() {  
   // Start serial connection
   Serial.begin(9600);
 
-  // Initialize control over the keyboard
+  // Initialise control over the keyboard
   Keyboard.begin();
-  
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo only
-  }
 
-  Serial.println("\n\nReady!");
-  Serial.print("Initial previousButtonState: ");
-  Serial.print(previousButtonState);
+  // Leondardo needs to wait for serial to be ready
+  while (!Serial); 
+  Serial.println("\nnf-language-button is ready!\n");
   
-  //configure pin2 as an input and enable the internal pull-up resistor
-  pinMode(buttonPin, INPUT_PULLUP);
-  pinMode(13, OUTPUT);
-
-  // initialize control over the keyboard:
-  //Keyboard.begin();
+  // Enable the internal pull-up resistor for the button pin
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
-  //read the pushbutton value into a variable
-  int buttonState = digitalRead(buttonPin);
-  //Serial.println(buttonState);
-
+  // How long have the board been running
+  elapsedTime = millis();
+  
+  // Handle the language button 
+  buttonState = digitalRead(BUTTON_PIN);
+  
   if (buttonState != previousButtonState) {
-    //Serial.println("\nButton state changed");
-    //Serial.println(buttonState);
-
     if (buttonState == HIGH) {
-      Serial.println("Send key!");
-      Keyboard.print("All meeting!");
+      // Button is pressed - change language
+      doLanguageToggle();
+      lastPressTime = elapsedTime;
     } else {
-      // HACK: Our buttons send one or more HIGHs on release. Ignore those!
+      // HACK: Our buttons send one or more HIGHs on release; ignore those!
       delay(50);
     }
   }
-  
 
-/*
-  if ((buttonState == HIGH) && (buttonState != previousButtonState)) {
-    // Button depressed
-    digitalWrite(13, HIGH);
-    Serial.println("\n\n --> Loop start: Send key!");
-    Serial.print("buttonState: ");
-    Serial.print(buttonState);
-    Serial.print("\nprevioiusButtonState: ");
-    Serial.print(previousButtonState);
-  } else {    
-    // Button open
-    digitalWrite(13, LOW);
+  // Check for long press
+  if (buttonState == HIGH && (elapsedTime - lastPressTime > LONG_PRESS_DURATION)) {
+    Serial.println("Long press!");
+    // Send engagement gesture and cancel autoEngagement on long press
+    doEngagementGesture();
+    autoEngagementEnabled = false;
+    lastPressTime = elapsedTime;
   }
-*/
 
   previousButtonState = buttonState; 
+
+  // Automatic engagement gesture on bootyp logic
+  if (elapsedTime > ENGAGEMENT_DURATION) {
+    autoEngagementEnabled = false;
+  }
+  
+  if (autoEngagementEnabled) {
+    if ((elapsedTime - lastEngagementTime) > ENGAGEMENT_INTERVAL) {
+      doEngagementGesture();
+      lastEngagementTime = elapsedTime;
+    } 
+  }
+
+  // Engagement gesture on languagelong press
+  elapsedTime = millis();
 }
+
+void doLanguageToggle() {
+  Serial.println("Toggle language");
+  Keyboard.write(LANGUAGE_CHAR);
+}
+
+void doEngagementGesture() {
+  Serial.println("Engagement gesture");
+  Keyboard.write(ENGAGEMENT_CHAR);
+}
+
 
 
 
