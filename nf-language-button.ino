a67ce90ae3980a2f32446544035260be725379d0
erik <erik@unswornindustries.com>
2015-10-14 10:45:10 +0200

    Better button logic. Version 0.0.3

--- nf-language-button.ino
+++ nf-language-button.ino
@@ -4,7 +4,10 @@
  See README.md in the git repo for details.
 */
 
-const String VERSION = "0.0.1";
+// Remove comment to enable debug output to Serial Monitorlpp
+//#define DEBUG
+
+const String VERSION = "0.0.3";
  
 // Pins used
 const int BUTTON_PIN = 2;
@@ -12,6 +15,9 @@ const int LED_PIN = 13;
 
 // How many milliseconds is considered a long press
 const int LONG_PRESS_DURATION = 3000;
+
+// Ingore button state changes for some ms to avoid false positives
+const int DEBOUNCE_TIME = 10;
 
 // This character is sent on button press
 const char LANGUAGE_CHAR = 'l';
@@ -31,19 +37,20 @@ boolean longPressEngagementEnabled = true;
 unsigned long elapsedTime = 0;
 unsigned long lastEngagementTime = 0;
 unsigned long lastPressTime = 0;
-int previousButtonState = LOW;
+int previousButtonState = HIGH;
 int buttonState;
 
 void setup() {  
-  // Start serial connection
-  Serial.begin(9600);
-
+  #ifdef DEBUG
+    // Start serial connection
+    Serial.begin(9600);
+    // Leondardo needs to wait for serial to be ready
+    while (!Serial); 
+    debug("\nnf-language-button version " + VERSION + " is ready!");
+  #endif
+  
   // Initialise control over the keyboard
   Keyboard.begin();
-
-  // Leondardo needs to wait for serial to be ready
-  while (!Serial); 
-  Serial.println("\nnf-language-button version " + VERSION + " is ready!");
   
   // Enable the internal pull-up resistor for the button pin
   pinMode(BUTTON_PIN, INPUT_PULLUP);
@@ -57,36 +64,34 @@ void loop() {
   // Handle the language button 
   buttonState = digitalRead(BUTTON_PIN);
   
-  if (buttonState != previousButtonState) {
-    if (buttonState == HIGH) {
-      // Button is pressed - change language
-      doLanguageToggle();
-      lastPressTime = elapsedTime;
-    } else {
-      // HACK: Our buttons send one or more HIGHs on release; ignore those!
-      delay(50);
-    }
-  }
-
-  // Check for long press
-  if (buttonState == HIGH && (elapsedTime - lastPressTime > LONG_PRESS_DURATION)) {
-    Serial.println("Long press!");
-    // Send engagement gesture and cancel autoEngagement on long press
-    doEngagementGesture();
-    autoEngagementEnabled = false;
+  if ((buttonState != previousButtonState) && buttonState == LOW) {
+    // Button is pressed - change language
+    debug("Toggle language");
+    Keyboard.write(LANGUAGE_CHAR);
+    delay(DEBOUNCE_TIME);
     lastPressTime = elapsedTime;
   }
 
   previousButtonState = buttonState; 
 
-  // Automatic engagement gesture on bootyp logic
+  // Check for long press
+  if (buttonState == LOW && (elapsedTime - lastPressTime > LONG_PRESS_DURATION)) {
+    // Send engagement gesture and cancel autoEngagement on long press
+    debug("Long press! Engagement gesture");
+    Keyboard.write(ENGAGEMENT_CHAR);
+    autoEngagementEnabled = false;
+    lastPressTime = elapsedTime;
+  }
+
+  // Automatic engagement gesture on bootup logic
   if (elapsedTime > ENGAGEMENT_DURATION) {
     autoEngagementEnabled = false;
   }
   
   if (autoEngagementEnabled) {
     if ((elapsedTime - lastEngagementTime) > ENGAGEMENT_INTERVAL) {
-      doEngagementGesture();
+      debug("Engagement gesture");
+      Keyboard.write(ENGAGEMENT_CHAR);
       lastEngagementTime = elapsedTime;
     } 
   }
@@ -95,16 +100,11 @@ void loop() {
   elapsedTime = millis();
 }
 
-void doLanguageToggle() {
-  Serial.println("Toggle language");
-  Keyboard.write(LANGUAGE_CHAR);
+void debug(String str) {
+  #ifdef DEBUG
+    Serial.println(str);
+  #endif
 }
-
-void doEngagementGesture() {
-  Serial.println("Engagement gesture");
-  Keyboard.write(ENGAGEMENT_CHAR);
-}
-
 
 
 
